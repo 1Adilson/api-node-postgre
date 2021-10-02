414f92e12596e9a2bb12a0c88795a36bc436b0db
Adilson Júnior <Juniorkadosh12@gmail.com>
2021-10-02 11:38:12 -0300

    inicia projeto

---
+++ api.js
@@ -0,0 +1,14 @@
+const inMemoryHorrors = [
+  {
+    name: "The Hills Have Eyes",
+    rating: 7.8,
+  },
+  {
+    name: "Night of the Living Dead",
+    rating: 9.0,
+  },
+  {
+    name: "Scream",
+    rating: 7.2,
+  },
+];
